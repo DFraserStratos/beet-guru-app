c033873d91f1b6cafbc386d704544f979107f8cb
DFraserStratos <d.fraser@stp.co.nz>
2025-05-09 12:05:16 +1200

    Fix mobile header alignment to be left-aligned like desktop

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,8 +4,8 @@ import { useState } from 'react';
 const Header = ({ activeScreen }) => {
   return (
     <div className="bg-green-800 text-white border-b border-green-700">
-      <div className="p-4 flex items-center justify-center">
-        {/* Logo and app name - centered */}
+      <div className="p-4 flex items-center">
+        {/* Logo and app name - left aligned */}
         <div className="flex items-center">
           <div className="bg-white rounded-full h-10 w-10 flex items-center justify-center mr-3">
             <div className="text-green-800 font-bold text-xl">B</div>
