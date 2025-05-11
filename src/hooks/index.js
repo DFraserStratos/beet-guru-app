5b342e6e960fe0d491f7d85134fcc1f53b954905
DFraserStratos <d.fraser@stp.co.nz>
2025-05-12 11:37:19 +1200

    Add hooks index file for easier importing

---
+++ src/hooks/index.js
@@ -0,0 +1,3 @@
+export { default as useDeviceDetection } from './useDeviceDetection';
+export { default as useForm } from './useForm';
+export { default as useLocalStorage } from './useLocalStorage';
