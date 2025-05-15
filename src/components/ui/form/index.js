82d2b4fcbddd2eadcfdb5ee2ca63d9792beebe9b
DFraserStratos <d.fraser@stp.co.nz>
2025-05-16 11:45:37 +1200

    Update form index.js to export all new components

--- src/components/ui/form/index.js
+++ src/components/ui/form/index.js
@@ -1,3 +1,17 @@
+// Export existing form components for backward compatibility
 export { default as FormField } from './FormField';
 export { default as FormButton } from './FormButton';
 export { default as FormButtonNav } from './FormButtonNav';
+
+// Export new form system components
+export { default as Form } from './Form';
+export { default as FormContext, FormProvider, useFormContext } from './FormContext';
+export { default as FormValidation } from './FormValidation';
+export { 
+  FormSection, 
+  FormInput, 
+  NumberField, 
+  ToggleField, 
+  DateField, 
+  ToggleTypeField 
+} from './FormFields';
